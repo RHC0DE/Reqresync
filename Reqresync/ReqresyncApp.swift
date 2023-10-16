3d14c28cce0f5c2a9876ed5f2e01898b0ed8e3d0
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-16 15:26:17 +0200

    Initial Commit

---
+++ Reqresync/ReqresyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReqresyncApp.swift
+//  Reqresync
+//
+//  Created by Guest User on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReqresyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
