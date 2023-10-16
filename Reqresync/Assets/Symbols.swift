b882d26bb2bdc70340c34a73e4dccd08ff59f26d
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-16 19:06:02 +0200

    Added an app icon, splash screen and added the map hierarchy

---
+++ Reqresync/Assets/Symbols.swift
@@ -0,0 +1,8 @@
+//
+//  Symbols.swift
+//  Reqresync
+//
+//  Created by Guest User on 16/10/2023.
+//
+
+import Foundation
---
+++ Reqresync/Presentation/UI/People/PeopleView.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleView.swift
+//  Reqresync
+//
+//  Created by Guest User on 16/10/2023.
+//
+
+import SwiftUI
+
+struct PeopleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeopleView()
+}
