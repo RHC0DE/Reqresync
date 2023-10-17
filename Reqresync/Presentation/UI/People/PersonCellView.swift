44a249826b7462a90cfd2a7156b8bc5d86472d21
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-17 22:43:39 +0200

    Created the PeopleView UI

---
+++ Reqresync/Presentation/UI/People/PersonCellView.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCellView.swift
+//  Reqresync
+//
+//  Created by Guest User on 17/10/2023.
+//
+
+import SwiftUI
+
+struct PersonCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonCellView()
+}
---
+++ Reqresync/Presentation/UI/Settingpage/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Reqresync
+//
+//  Created by Guest User on 17/10/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
