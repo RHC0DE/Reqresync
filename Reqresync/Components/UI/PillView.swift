20ae83bf42ee42cb3d3c0129cde31414be27bcdf
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-17 23:26:16 +0200

    Extracted view items into components

---
+++ Reqresync/Components/UI/PillView.swift
@@ -0,0 +1,18 @@
+//
+//  PillView.swift
+//  Reqresync
+//
+//  Created by Guest User on 17/10/2023.
+//
+
+import SwiftUI
+
+struct PillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PillView()
+}
