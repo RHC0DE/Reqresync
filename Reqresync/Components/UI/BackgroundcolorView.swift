5385d4c92bc11e1734cd9af2eccfa4eae24c99eb
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-20 16:50:48 +0200

    Created the DetailView UI

---
+++ Reqresync/Components/UI/BackgroundcolorView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundcolorView.swift
+//  Reqresync
+//
+//  Created by Guest User on 18/10/2023.
+//
+
+import SwiftUI
+
+struct BackgroundcolorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundcolorView()
+}
