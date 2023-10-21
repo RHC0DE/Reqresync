2c7161d9ceb43915e9fa7fb37c465707ee951303
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-21 18:17:11 +0200

    Implemented the CreatePersonView UI

---
+++ Reqresync/Presentation/UI/People/CreatePersonView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePersonView.swift
+//  Reqresync
+//
+//  Created by Guest User on 21/10/2023.
+//
+
+import SwiftUI
+
+struct CreatePersonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePersonView()
+}
