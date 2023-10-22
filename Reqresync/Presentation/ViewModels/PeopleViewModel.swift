818841536b44e8624cd547aebb959b4dd8d6041a
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-22 11:49:22 +0200

    Created the ViewModels 'PeopleViewModel' and 'PersonDetailViewModel'. The whole idea of clean code in Swift is that the View does not contain any form of technical code in it. So all the network calls are now handled in the right ViewModels.

---
+++ Reqresync/Presentation/ViewModels/PeopleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PeopleViewModel.swift
+//  Reqresync
+//
+//  Created by Guest User on 22/10/2023.
+//
+
+import Foundation
---
+++ Reqresync/Presentation/ViewModels/PersonDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PersonDetailViewModel.swift
+//  Reqresync
+//
+//  Created by Guest User on 22/10/2023.
+//
+
+import Foundation
