c9a3828885a7e688fd6cb5ecdc9d596d6251aa14
R H <96940361+RHC0DE@users.noreply.github.com>
2023-10-27 10:46:13 +0200

    Created a extra function in the NetworkingManager that is overriding the request function. The new request function is used for POST  request that do not require data to be decoded, but only checks whether the request has been successfully executed.

---
+++ Reqresync/Data/Models/UserCreated.swift
@@ -0,0 +1,8 @@
+//
+//  UserCreated.swift
+//  Reqresync
+//
+//  Created by Guest User on 27/10/2023.
+//
+
+import Foundation
---
+++ Reqresync/Presentation/ViewModels/CreatePersonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreatePersonViewModel.swift
+//  Reqresync
+//
+//  Created by Guest User on 27/10/2023.
+//
+
+import Foundation
