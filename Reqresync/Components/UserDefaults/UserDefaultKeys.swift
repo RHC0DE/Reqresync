74a00aa391f016d32a0e35a9ebef9d45552ff0a9
R H <96940361+RHC0DE@users.noreply.github.com>
2023-11-22 22:49:06 +0100

    Added Haptic feedback when the user Form is succesfully filled in. I Also implemented the Settings page where the user has the option to toggle Haptic feedback on or off

---
+++ Reqresync/Components/UserDefaults/UserDefaultKeys.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultKeys.swift
+//  Reqresync
+//
+//  Created by Guest User on 22/11/2023.
+//
+
+import Foundation
+
+enum UserDefaultKeys {
+    static let hapticEnabled = "hapticEnabled"
+}
---
+++ Reqresync/Presentation/Haptic/HapticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticsManager.swift
+//  Reqresync
+//
+//  Created by Guest User on 22/11/2023.
+//
+
+import Foundation
